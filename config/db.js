072d47df388504012f64acde55956fa7b282bfb0
infolanze807 <techteam.infolanze@gmail.com>
2025-09-05 12:16:33 +0530

    updated

--- config/db.js
+++ config/db.js
@@ -3,7 +3,9 @@ const mongoose = require('mongoose');
 async function connect() {
   await mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 30000,
+
   });
   console.log('MongoDB connected');
 }
