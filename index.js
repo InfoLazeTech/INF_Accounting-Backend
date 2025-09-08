b90c3674e8aec404702f04aedf9214f957d8b07b
infolanze807 <techteam.infolanze@gmail.com>
2025-09-08 11:17:56 +0530

    updated index.js

--- index.js
+++ index.js
@@ -1,35 +1,3 @@
-// require('dotenv').config();
-// const express = require('express');
-// const bodyParser = require('body-parser');
-// const routes = require('./routes');
-// // const errorHandler = require('./middlewares/errorHandler');
-// const { connect } = require('./config/db');
-
-// const PORT = process.env.PORT || 5000;
-// const MONGO_URI = process.env.MONGO_URI;
-// const SESSION_SECRET = process.env.SESSION_SECRET;
-
-// (async () => {
-//   try {
-//     await connect();
-
-//     const app = express();
-//     app.use(bodyParser.json());
-
-//     app.use('/', routes);
-//     // app.use(errorHandler);
-
-//     app.get("/", (req, res) => {
-//   res.send("Backend is running...");
-// });
-
-//     app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
-//   } catch (e) {
-//     console.error('Failed to start server', e);
-//     process.exit(1);
-//   }
-// })();
-
 const express = require('express');
 const cors = require("cors");
 const bodyParser = require('body-parser');
